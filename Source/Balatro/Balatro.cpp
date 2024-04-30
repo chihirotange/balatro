08ace42ff032661943c32eb43fa5b473c24f08d8
ai-chan <ohanasakuiroha@gmail.com>
2024-05-01 02:01:36 +0700

    init

---
+++ Source/Balatro/Balatro.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Balatro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Balatro, "Balatro" );
