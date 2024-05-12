04f266e4310d6cb5cac0bd241422791da9748170
ai-chan <ohanasakuiroha@gmail.com>
2024-05-13 01:44:46 +0700

    removed cpp file of interfaces

--- Source/Balatro/Private/Gameplay/Usable.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Gameplay/Usable.h"
-
-// Add default functionality here for any IUsable functions that are not pure virtual.
