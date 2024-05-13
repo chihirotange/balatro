ee03dec69ea2d6d0f6be7669ede0575a30c9ef0f
Chii chan <ai.chan@sipher.xyz>
2024-05-13 11:49:17 +0700

    renamed variable due to conflict with function param

--- Source/Balatro/Public/Card/PlayingCardData.h
+++ Source/Balatro/Public/Card/PlayingCardData.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Card/CardData.h"
+#include "Gameplay/HasSuit.h"
 #include "PlayingCardData.generated.h"
 
 enum class ECardSuit : uint8;
@@ -11,9 +12,13 @@ enum class ECardSuit : uint8;
  * 
  */
 UCLASS()
-class BALATRO_API UPlayingCardData : public UCardData, public IHasValue
+class BALATRO_API UPlayingCardData : public UCardData, public IHasValue, public IHasSuit
 {
 public:
+	virtual void SetSuit_Implementation(ECardSuit Suit) override;
+
+	virtual ECardSuit GetSuit_Implementation() override;
+
 	virtual int32 GetValue_Implementation() override;
 
 private:
@@ -23,5 +28,5 @@ private:
 	int32 CardValue;
 
 	UPROPERTY(EditAnywhere)
-	ECardSuit Suit;
+	ECardSuit CardSuit;
 };
